ad9792f154ba2f1e55d3b899076db378863b951d
Tobias Roenningen <tobias.ronningen@gmail.com>
2015-05-29 12:17:45 +0200

    added a function for message to models.py

--- kvittr/kvittr_tweets/models.py
+++ kvittr/kvittr_tweets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    message = models.TextField()
+    created_datetime = models.DateTimeField()
+    created_by = models.ForeignKey(User, related_name="created_message")
+    likes = models.PositiveIntegerField(default=0)
+
+    def __unicode__(self):
+        return u'%s' % self.message
+
+    class Meta:
+        ordering = ['-id']
