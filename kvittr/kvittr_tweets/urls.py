1df54a3e44b145cbb24e909f853888b3d3dbfbcf
Tobias Roenningen <tobias.ronningen@gmail.com>
2015-05-29 13:14:33 +0200

    made migrations, and synced db, modified urls.py

--- kvittr/kvittr_tweets/urls.py
+++ kvittr/kvittr_tweets/urls.py
@@ -5,4 +5,8 @@ from kvittr_tweets import views
 urlpatterns = patterns('',
     url(r'^$', views.message_listing, name='message_listing'),
     url(r'^(\d+)/$', views.message_details, name='message_details'),
- 
+   # url(r'^(\d+)/increase_passed_exams$', views.student_increase_passed_exams, 
+    #    name='student_increase_passed_exams'),   
+    #url(r'^(\d+)/add_points/(\d+)$', views.student_add_points, 
+     #   name='student_add_points'),   
+)
